116cbc54420b8c76c7d93cccb3be50fe87d4642b
Sai Charan <saicharan@gmail.com>
2026-04-04 22:56:49 +0530

    fix vercel routing

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,12 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+if (process.env.NODE_ENV !== 'production') {
+    connectDB().then(() => {
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    });
+} else {
+    connectDB();
+}
+
+export default app;
