53d8ae2e94bb00e96a3ae380f2e76e332b75fe86
Sai Charan <saicharan@gmail.com>
2026-04-04 22:13:05 +0530

    fix vercel routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://backend-5u6q.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
